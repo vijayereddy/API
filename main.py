9e0cc9210cda24fbf542b83d2c8cabaa21c6626d
vijayeshwar26 <vijayeshwar26@gmail.com>
2020-09-09 22:57:58 +0530

    update on the API output

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import flask
+from flask import jsonify
+import os
 from zipfile import ZipFile
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-
 @app.route('/', methods=['GET'])
 def home():
-    file_name = "zip.zip"
-    with ZipFile(file_name, 'r') as zip:
-        zip.printdir()
-        zip.extractall()
+    path = 'extracted/'
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.csv' in file:
+                files.append(os.path.join(r, file))
+    return jsonify({'response':files})
+
+file_name = "zip.zip"
+with ZipFile(file_name, 'r') as zip:
+    zip.extractall('extracted')
+
 app.run()

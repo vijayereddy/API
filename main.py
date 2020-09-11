672015334af14069ad30fde71c961cafdaf0ae71
vijayeshwar26 <vijayeshwar26@gmail.com>
2020-09-11 09:54:23 +0530

    comments removed

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def allowed_file(filename):
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    # check if the post request has the file part
     if 'file' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
         return resp

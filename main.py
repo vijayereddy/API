339bc7e8a86ee7756ba35cc89bb55ad21fb4e38f
vijayeshwar26 <vijayeshwar26@gmail.com>
2020-09-11 08:05:44 +0530

    a minor update on url

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/file-upload', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     # check if the post request has the file part
     if 'file' not in request.files:
